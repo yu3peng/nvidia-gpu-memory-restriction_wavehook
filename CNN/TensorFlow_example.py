be60e70afb446f7fe0363c8380d1804435b834c1
yupeng <yu3peng@qq.com>
2021-04-26 19:01:56 +0800

    Update TensorFlow_example.py

--- CNN/TensorFlow_example.py
+++ CNN/TensorFlow_example.py
@@ -2,7 +2,7 @@ import tensorflow as tf
  
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
-    if(logs.get('accuracy')>0.99999999999999999):
+    if(logs.get('accuracy')>0.9999):
       print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
  
