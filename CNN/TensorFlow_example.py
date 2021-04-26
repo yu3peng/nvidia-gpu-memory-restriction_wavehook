57c2063eb81946cd9e99e0dc89fce530069dd199
yupeng <yu3peng@qq.com>
2021-04-26 16:55:53 +0800

    Update TensorFlow_example.py

--- CNN/TensorFlow_example.py
+++ CNN/TensorFlow_example.py
@@ -1,64 +1,55 @@
-# -*- coding: UTF-8 -*-
-# [TensorFlow平台下Python实现神经网络](https://www.yisu.com/zixun/179765.html)
-
-import tensorflow as tf 
-#导入TensorFlow工具包并简称为tf 
+import tensorflow as tf
+import os
+import zipfile
  
-from numpy.random import RandomState 
-#导入numpy工具包，生成模拟数据集 
+DESIRED_ACCURACY = 0.999   #desired accuracy 
  
-batch_size = 10 
-#定义训练数据batch的大小 
+!wget --no-check-certificate \
+    "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip" \
+    -O "/tmp/happy-or-sad.zip"
  
-w1 = tf.Variable(tf.random.uniform([2,3],stddev=1,seed=1)) 
-w2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1)) 
-#分别定义一二层和二三层之间的网络参数，标准差为1，随机产生的数保持一致 
+zip_ref = zipfile.ZipFile("/tmp/happy-or-sad.zip", 'r')
+zip_ref.extractall("/tmp/h-or-s")
+zip_ref.close()
  
-x = tf.placeholder(tf.float32,shape=(None,2),name='x-input') 
-y_ = tf.placeholder(tf.float32,shape=(None,1),name='y-input') 
-#输入为两个维度，即两个特征，输出为一个标签,声明数据类型float32，None即一个batch大小 
-#y_是真实的标签 
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>DESIRED_ACCURACY):
+      print("\nReached 99.9% accuracy so cancelling training!")
+      self.model.stop_training = True
  
-a = tf.matmul(x,w1) 
-y = tf.matmul(a,w2) 
-#定义神经网络前向传播过程 
+callbacks = myCallback()
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
  
-cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y,1e-10,1.0))) 
-train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy) 
-#定义损失函数和反向传播算法 
+from tensorflow.keras.optimizers import RMSprop
  
-rdm = RandomState(1) 
-dataset_size = 128 
-#产生128组数据 
-X = rdm.rand(dataset_size,2) 
-Y = [[int(x1+x2 < 1)] for (x1,x2) in X] 
-#将所有x1+x2<1的样本视为正样本，表示为1；其余为0 
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
  
-#创建会话来运行TensorFlow程序 
-with tf.Session() as sess: 
- init_op = tf.global_variables_initializer() 
- #初始化变量 
- sess.run(init_op) 
+train_datagen = ImageDataGenerator(rescale=1/255)
  
- print(sess.run(w1)) 
- print(sess.run(w2)) 
- #打印出训练网络之前网络参数的值 
+train_generator = train_datagen.flow_from_directory(
+        "/tmp/h-or-s",  
+        target_size=(150, 150), 
+        batch_size=10,
+        class_mode='binary')
  
- STEPS = 500000 
- #设置训练的轮数 
- for i in range(STEPS): 
-  start = (i * batch_size) % dataset_size 
-  end = min(start+batch_size,dataset_size) 
- #每次选取batch_size个样本进行训练 
-  
-  sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]}) 
- #通过选取的样本训练神经网络并更新参数 
-  
-  if i%1000 == 0: 
-   total_cross_entropy = sess.run(cross_entropy,feed_dict={x:X,y_:Y}) 
-   print("After %d training step(s),cross entropy on all data is %g" % (i,total_cross_entropy)) 
- #每隔一段时间计算在所有数据上的交叉熵并输出，随着训练的进行，交叉熵逐渐变小 
- 
- print(sess.run(w1)) 
- print(sess.run(w2)) 
- #打印出训练之后神经网络参数的值 
+# Expected output: 'Found 80 images belonging to 2 classes'
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=2,  
+      epochs=15,
+      verbose=1,
+      callbacks=[callbacks])
