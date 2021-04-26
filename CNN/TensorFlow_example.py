e76b8ddd4438dacaf1133cf41afdddb72e75dcef
yupeng <yu3peng@qq.com>
2021-04-26 17:53:00 +0800

    Update TensorFlow_example.py

--- CNN/TensorFlow_example.py
+++ CNN/TensorFlow_example.py
@@ -1,55 +1,25 @@
 import tensorflow as tf
-import os
-import zipfile
- 
-DESIRED_ACCURACY = 0.999   #desired accuracy 
- 
-!wget --no-check-certificate \
-    "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip" \
-    -O "/tmp/happy-or-sad.zip"
- 
-zip_ref = zipfile.ZipFile("/tmp/happy-or-sad.zip", 'r')
-zip_ref.extractall("/tmp/h-or-s")
-zip_ref.close()
  
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
-    if(logs.get('acc')>DESIRED_ACCURACY):
-      print("\nReached 99.9% accuracy so cancelling training!")
+    if(logs.get('acc')>0.99):
+      print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
  
+mnist = tf.keras.datasets.mnist
+ 
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+ 
 callbacks = myCallback()
+ 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
  
-from tensorflow.keras.optimizers import RMSprop
- 
-model.compile(loss='binary_crossentropy',
-              optimizer=RMSprop(lr=0.001),
-              metrics=['acc'])
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
- 
-train_datagen = ImageDataGenerator(rescale=1/255)
- 
-train_generator = train_datagen.flow_from_directory(
-        "/tmp/h-or-s",  
-        target_size=(150, 150), 
-        batch_size=10,
-        class_mode='binary')
- 
-# Expected output: 'Found 80 images belonging to 2 classes'
-history = model.fit_generator(
-      train_generator,
-      steps_per_epoch=2,  
-      epochs=15,
-      verbose=1,
-      callbacks=[callbacks])
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
