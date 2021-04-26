a45bf7b3142ea77605cf5232fe3506696f88add5
yupeng <yu3peng@qq.com>
2021-04-26 18:05:32 +0800

    Update TensorFlow_example.py

--- CNN/TensorFlow_example.py
+++ CNN/TensorFlow_example.py
@@ -2,7 +2,7 @@ import tensorflow as tf
  
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
-    if(logs.get('acc')>0.99):
+    if(logs.get('accuracy')>0.99999999999999999):
       print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
  
@@ -16,10 +16,10 @@ callbacks = myCallback()
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+  tf.keras.layers.Dense(100, activation=tf.nn.softmax)
 ])
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
  
-model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+model.fit(x_train, y_train, epochs=100, callbacks=[callbacks])
